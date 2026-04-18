4efefea980e8a2d557bb252987f54eb4fd7308ea
TH1RT3EN <litianshun.cn@gmail.com>
2026-04-18 19:54:40 +0800

    test 20.0

--- src/lslidar_driver/lslidar_driver/launch/lslidar_launch.py
+++ src/lslidar_driver/lslidar_driver/launch/lslidar_launch.py
@@ -2,15 +2,10 @@
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import LifecycleNode
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
 
-import lifecycle_msgs.msg
 import os
 
 def generate_launch_description():
-
     driver_dir = os.path.join(get_package_share_directory('lslidar_driver'), 'params', 'lsx10.yaml')
                      
     driver_node = LifecycleNode(package='lslidar_driver',
@@ -37,4 +32,3 @@ def generate_launch_description():
         driver_node,
         # rviz_node,
     ])
-
